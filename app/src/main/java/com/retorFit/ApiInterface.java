0b91d30e0f8e4e77d782da5cb15473b794c538f2
Souarv Sharma <souravgopal25@gmail.com>
2020-07-11 02:07:41 +0530

    Update ApiInterface.java

--- app/src/main/java/com/retorFit/ApiInterface.java
+++ app/src/main/java/com/retorFit/ApiInterface.java
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
-//https://api.themoviedb.org/3/movie/popular?api_key=c116a273a05858824b03df6c4cfcbf20&language=en-US&page=1
+
 public interface ApiInterface {
     @GET("/3/movie/{category}")
     Call<MovieResults>listofMovies(@Path("category") String category,
